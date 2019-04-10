c0a11e131924c84e88a6dcc15ec6c9ab14710ed8
f0nzie zbook G4 <alfonso.reyes@oilgainsanalytics.com>
2019-04-10 01:31:11 -0500

    customizing setup

---
+++ setup.py
@@ -0,0 +1,23 @@
+# from setuptools import setup
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+
+setuptools.setup(
+    name="restools",
+    version="0.0.1",
+    author="Konstantin Sermyagin, Alfonso Reyes",
+    author_email="kvs@readas.net, alfonso.reyes@oilgainsanalytics.com",
+    description="An Eclipse reservoir simulation binary files reader",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/f0nzie/restools",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
